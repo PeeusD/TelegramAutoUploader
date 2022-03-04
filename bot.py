e938fc5b30a7fc9dd8e2294ac540b141001c7c4f
PeeusD <2014pgmca076@nitjsr.ac.in>
2022-03-04 10:15:10 +0530

    added sleep for handling request

--- bot.py
+++ bot.py
@@ -61,6 +61,7 @@ async def handler(event):
     # filtering documents
     if event.document:
         if 'TH-DELHI' in event.file.name.upper():
+            await asyncio.sleep(5)
             await event.download_media(file=open(event.file.name, "wb"))
             pdf_mgmt(event.file.name)
             await client.send_file(MY_CHAT_ID,file= open(event.file.name, 'rb'), thumb='thumb.jpg')
@@ -73,8 +74,10 @@ async def handler(event):
             remove(event.file.name)
 
         elif 'IE DELHI' in event.file.name.upper():
+            await asyncio.sleep(5)
             await event.download_media(file=open(event.file.name, "wb"))
             pdf_mgmt(event.file.name)
+            
             await client.send_file(MY_CHAT_ID, open(event.file.name, 'rb'), thumb='thumb.jpg')
             await client.send_file(MY_CHAT_ID2, open(event.file.name, 'rb'), thumb='thumb.jpg')
            
@@ -84,8 +87,10 @@ async def handler(event):
             remove(event.file.name)
 
         elif 'MAGAZINE' in event.file.name.upper():
+            await asyncio.sleep(5)
             await event.download_media(file=open(event.file.name, "wb"))
             pdf_mgmt(event.file.name)
+            
             await client.send_file(MY_CHAT_ID, open(event.file.name, 'rb'), thumb='thumb.jpg')
             await client.send_file(MY_CHAT_ID2, open(event.file.name, 'rb'), thumb='thumb.jpg')
             await client.send_file(BOT_CHAT_ID, open(event.file.name, 'rb'), thumb='thumb.jpg')
@@ -98,15 +103,17 @@ async def handler(event):
         else:
             if not event.sticker:
                 #forwarding files
-                await client.send_file(MY_CHAT_ID, event.message, schedule=datetime.timedelta(minutes=2))
-                await client.send_file(MY_CHAT_ID2, event.message, schedule=datetime.timedelta(minutes=2))
+                # await client.send_file(MY_CHAT_ID, event.message, schedule=datetime.timedelta(minutes=2))
+                await asyncio.sleep(5)
+                await client.send_file(MY_CHAT_ID, event.message,)
+                await client.send_file(MY_CHAT_ID2, event.message,)
                 m = await client.send_message(PD_CHAT_ID, f'Uploaded {event.file.name}')
                 if 'TH' in event.file.name.upper() :
                     # forwarding files
                     await client.send_file(BOT_CHAT_ID, event.message)
                     await client.send_file(BOT_CHAT_ID2, event.message)
                 
-                # await asyncio.sleep(10)
+                
                 # await client.delete_messages(PD_CHAT_ID, [m.id])
 
 
